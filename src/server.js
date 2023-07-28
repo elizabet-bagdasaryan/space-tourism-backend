660048a52dfaa338fa99ce050b5b3ad64081050d
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-07-28 13:37:28 +0400

    feat: connected to the database

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 import express from "express";
 import cors from "cors";
-
+import { connection } from "./database/mongo.js";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
 const app = express();
+dotenv.config();
+connection();
 
+app.use(bodyParser.json());
 app.use(cors());
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is listening on port 3000");
+});
