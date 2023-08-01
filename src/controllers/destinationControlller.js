df6c0d020a8c1cbceac5d77daf7a3faef70724b2
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-08-01 19:45:20 +0400

    fix: fix destination controller

--- src/controllers/destinationControlller.js
+++ src/controllers/destinationControlller.js
@@ -4,7 +4,7 @@ const getDestination = async (req, res) => {
   try {
     const { name } = req.params;
     const destination = await destinationData.findOne({ name });
-    res.status(200).json({ message: "Data retrieved successfully", crew });
+    res.status(200).json({ message: "Data retrieved successfully", destination});
   } catch (error) {
     res.status(500).json({ message: "Erorr", err: error });
   }
