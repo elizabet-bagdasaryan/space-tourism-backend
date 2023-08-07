9168e54fddca9341b2e908bc28e342af83d47ac9
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-08-07 17:27:32 +0400

    feat: add swagger configuration

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { connection } from "./database/mongo.js";
+import { connection } from "./config/mongo.js";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import getCrew from "./controllers/crewController.js";
