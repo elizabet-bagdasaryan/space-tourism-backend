65f0cad3369f4564701e96befa79a32b65134ad8
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-08-08 00:03:13 +0400

    fix: fix cors usage

--- src/middleware/swagger-middleware.js
+++ src/middleware/swagger-middleware.js
@@ -4,7 +4,7 @@ import YAML from "yamljs";
 const swaggerMiddleware = () => {
   const options = {
     customCss: ".swagger-ui .topbar{ display:none}",
-    customSiteTitle: "ToDo",
+    customSiteTitle: "SpaceTourism",
   };
   const swaggerDocument = YAML.load("./src/config/swagger.yaml");
   return [SwaggerUI.serve, SwaggerUI.setup(swaggerDocument, options)];
